7b20fb977233757819490f720ec705bead44529c
jhomble <jhomble7@gmail.com>
2017-08-14 23:43:18 -0400

    Setup react

---
+++ app/components/app.js
@@ -0,0 +1,13 @@
+var React = require('react')
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>It Works</h1>
+            </div>
+        );
+    }
+}
+
+module.exports = App
