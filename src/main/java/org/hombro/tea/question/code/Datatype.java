a5ccbae7526d0f36840f583c8b1f4342a4bc0e41
nicolas <nhomble@terpmail.umd.edu>
2017-09-11 22:58:49 -0400

    convert to the language's datatype name

--- src/main/java/org/hombro/tea/question/code/Datatype.java
+++ src/main/java/org/hombro/tea/question/code/Datatype.java
@@ -7,5 +7,28 @@ public enum Datatype {
     INTEGER,
     DOUBLE,
     STRING,
-    NONE
+    NONE;
+
+    public String forLanguage(Language l) {
+        if (l.equals(Language.JAVA)) {
+            String type;
+            switch (this) {
+                case DOUBLE:
+                    type = "double";
+                    break;
+                case INTEGER:
+                    type = "int";
+                    break;
+                case STRING:
+                    type = "String";
+                    break;
+                default:
+                    type = "void";
+            }
+            return type;
+        }
+        else{
+            return "";
+        }
+    }
 }
